351fbf00166535e88a63f58d58db75a5002d68a0
senails <seffele@gmail.com>
2023-05-31 22:56:19 +0300

    add block with Text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,50 @@
 import { HoverPanel } from "@/Shared/components/HoverPanel";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main>
       <HoverPanel/>
+      <div className={styles.wall}>
+        <div className={styles.wall2}>
+            Потенциальная зона с контентом
+        </div>
+      </div>
+      <div className={styles.wall}>
+        <div className={styles.wall2}>
+            Потенциальная зона с контентом
+        </div>
+      </div>
+      <div className={styles.wall}>
+        <div className={styles.wall2}>
+            Потенциальная зона с контентом
+        </div>
+      </div>
+      <div className={styles.wall}>
+        <div className={styles.wall2}>
+            Потенциальная зона с контентом
+        </div>
+      </div>
+      <div className={styles.wall}>
+        <div className={styles.wall2}>
+            Потенциальная зона с контентом
+        </div>
+      </div>
+      <div className={styles.wall}>
+        <div className={styles.wall2}>
+            Потенциальная зона с контентом
+        </div>
+      </div>
+      <div className={styles.wall}>
+        <div className={styles.wall2}>
+            Потенциальная зона с контентом
+        </div>
+      </div>
+      <div className={styles.wall}>
+        <div className={styles.wall2}>
+            Потенциальная зона с контентом
+        </div>
+      </div>
     </main>
   )
 }
